f63af33cdb28418ede631e94e8e796642a8a7228
Josef <josef.matti05@gmail.com>
2024-10-22 11:56:43 +0200

    update

---
+++ src/components/card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="profile-card">
+      <img src="" alt="profile" />
+
+      <h2 className="name">Josef Matti</h2>
+      <p className="title">Frontend Developer</p>
+      <button className="contact-btn"></button>
+    </div>
+  );
+}
+
+export default Card;
